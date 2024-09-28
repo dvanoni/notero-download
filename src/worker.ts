2ea96aadd3417b42cc5ec6170cb978647f000ec6
David Hoff-Vanoni <david@vanoni.dev>
2024-09-28 14:18:18 -0700

    feat: Cache response for 5 minutes

--- src/worker.ts
+++ src/worker.ts
@@ -2,17 +2,32 @@ import type { components } from '@octokit/openapi-types';
 
 type Release = components['schemas']['release'];
 
+const CACHE_TTL = 300; // 5 minutes
+
+function logRateLimitHeaders(res: Response) {
+  const headers = Array.from(res.headers.entries());
+  const rateLimitHeaders = headers.filter(([key]) =>
+    key.startsWith('x-ratelimit'),
+  );
+  console.log(Object.fromEntries(rateLimitHeaders));
+}
+
 async function fetchLatestDownloadUrl(): Promise<string | null> {
+  console.log('Fetching latest release');
   const res = await fetch(
     'https://api.github.com/repos/dvanoni/notero/releases/latest',
     {
+      cf: {
+        cacheTtlByStatus: { '200-299': CACHE_TTL, 404: 1, '500-599': 0 },
+      },
       headers: {
         Accept: 'application/vnd.github+json',
-        'User-Agent': 'notero-download',
+        'User-Agent': 'dvanoni/notero-download',
         'X-GitHub-Api-Version': '2022-11-28',
       },
     },
   );
+  logRateLimitHeaders(res);
   const json = await res.json();
   if (typeof json !== 'object' || json === null || !('assets' in json)) {
     return null;
@@ -21,12 +36,40 @@ async function fetchLatestDownloadUrl(): Promise<string | null> {
   return release.assets[0]?.browser_download_url || null;
 }
 
+async function buildResponse(): Promise<Response> {
+  const downloadUrl = await fetchLatestDownloadUrl();
+  if (!downloadUrl) {
+    return new Response('Failed to fetch latest release', {
+      headers: { 'Cache-Control': 'no-cache' },
+      status: 500,
+    });
+  }
+  let response = Response.redirect(downloadUrl, 302);
+  response = new Response(response.body, response);
+  response.headers.set('Cache-Control', `s-maxage=${CACHE_TTL}`);
+  return response;
+}
+
 export default {
   async fetch(request, env, ctx) {
-    const downloadUrl = await fetchLatestDownloadUrl();
-    if (!downloadUrl) {
-      return new Response('Failed to fetch latest release', { status: 500 });
+    const url = new URL(request.url);
+    if (url.pathname !== '/') {
+      return new Response('Not found', { status: 404 });
     }
-    return Response.redirect(downloadUrl, 302);
+
+    const cacheKey = new Request(url.toString(), request);
+    const cache = caches.default;
+
+    let response = await cache.match(cacheKey);
+
+    if (response) {
+      console.log(`Cache hit for: ${request.url}`);
+    } else {
+      console.log(`Cache miss for: ${request.url}`);
+      response = await buildResponse();
+      ctx.waitUntil(cache.put(cacheKey, response.clone()));
+    }
+
+    return response;
   },
 } satisfies ExportedHandler<Env>;
